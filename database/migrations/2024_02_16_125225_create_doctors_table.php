e34d19f8d34708cbdecbee2c1649a5501e522bde
Mohamed20a <m16ahmed17@gmail.com>
2024-02-16 21:30:38 +0200

    First commit

---
+++ database/migrations/2024_02_16_125225_create_doctors_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('card_img')->nullable();
+            $table->unsignedBigInteger('specialty_id')->nullable();
+            $table->string('education');
+            $table->string('experience');
+            $table->string('day_work')->nullable();
+            $table->time('time_from')->nullable();
+            $table->time('time_to')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->timestamps();
+
+            $table->foreign('specialty_id')->references('id')->on('specialty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
